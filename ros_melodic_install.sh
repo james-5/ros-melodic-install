9b3c7264049eb965a8d47da6f8d0c963b522e3ad
james-5 <jamesthurman473@gmail.com>
2019-03-22 09:15:25 -0500

    Add files via upload

--- ros_melodic_install.sh
+++ ros_melodic_install.sh
@@ -13,11 +13,20 @@ echo this is optomized for ubuntu bionic
 echo this is optomized for ubuntu bionic
 echo this is optomized for ubuntu bionic
 echo this is optomized for ubuntu bionic
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list'
+wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+sudo apt-get install python-catkin-tools -y
+sudo apt-get install python-rostopic -y
+sudo apt-get install arduino -y
+sudo apt-get install ros-melodic-cv-bridge -y
+sudo apt-get install ros-melodic-vision-opencv -y
+sudo apt install rviz -y
 cd
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-sudo apt update
-sudo apt install ros-melodic-desktop-full
+sudo apt-get install ros-melodic-ros-tutorials -y
+sudo apt install ros-melodic-desktop-full -y 
+sudo apt-get install python-cv-bridge -y
 apt search ros-melodic
 sudo rosdep init
 sudo rosdep fix-permissions
@@ -58,3 +67,4 @@ echo to start rqt-gui rype rqt
 echo to start rqt-gui rype rqt
 echo to start rqt-gui rype rqt
 echo to start rqt-gui rype rqt
+sudo sh ./cv4.sh
