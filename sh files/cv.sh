d9ba401e6dd7688b5f0af33bf5e5ee8d1d673647
james-5 <jamesthurman473@gmail.com>
2019-04-12 07:20:04 -0700

    Add files via upload

---
+++ sh files/cv.sh
@@ -0,0 +1,57 @@
+
+#!bin/bash
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install build-essential cmake unzip pkg-config
+sudo apt-get install libjpeg-dev libpng-dev libtiff-dev
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install libxvidcore-dev libx264-dev
+sudo apt-get install libgtk-3-dev
+sudo apt-get install libatlas-base-dev gfortran
+sudo apt-get install python3-dev
+cd ~
+wget -O opencv.zip https://github.com/opencv/opencv/archive/3.4.4.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/3.4.4.zip
+unzip opencv.zip
+unzip opencv_contrib.zip
+mv opencv-3.4.4 opencv
+mv opencv_contrib-3.4.4 opencv_contrib
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3 get-pip.py
+sudo pip install virtualenv virtualenvwrapper
+sudo rm -rf ~/get-pip.py ~/.cache/pip
+echo -e "\n# virtualenv and virtualenvwrapper" >> ~/.bashrc
+echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.bashrc
+echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> ~/.bashrc
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+source ~/.bashrc
+mkvirtualenv cv -p python3
+workon cv
+pip install numpy
+workon cv
+cd ~/opencv
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+	-D CMAKE_INSTALL_PREFIX=/usr/local \
+	-D INSTALL_PYTHON_EXAMPLES=ON \
+	-D INSTALL_C_EXAMPLES=OFF \
+	-D OPENCV_ENABLE_NONFREE=ON \
+	-D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+	-D PYTHON_EXECUTABLE=~/.virtualenvs/cv/bin/python \
+	-D BUILD_EXAMPLES=ON ..
+make -j4
+sudo make install
+sudo ldconfig
+cd /usr/local/python/cv2/python-3.6
+sudo mv cv2.cpython-36m-x86_64-linux-gnu.so cv2.so
+cd ~/.virtualenvs/cv/lib/python3.6/site-packages/
+ln -s /usr/local/python/cv2/python-3.6/cv2.so cv2.so
+cd ~
+workon cv
+python
+#in the python SHell type import cv2
+#when done enter these comands 
+# cd ~
+# rm opencv.zip opencv_contrib.zip
+# rm -rf opencv opencv_contrib
---
+++ sh files/dep.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+# RUN IN SU BY USING SUDO SU !!!!!!!!!!!	
+# RUN IN SU BY USING SUDO SU !!!!!!!!!!!	
+# RUN IN SU BY USING SUDO SU !!!!!!!!!!!	
+# RUN IN SU BY USING SUDO SU !!!!!!!!!!!	
+# RUN IN SU BY USING SUDO SU !!!!!!!!!!!	
+# RUN IN SU BY USING SUDO SU !!!!!!!!!!!	
+# RUN IN SU BY USING SUDO SU !!!!!!!!!!!	\
+
+sudo apt-get install ros-melodic-rosserial-arduino
+
+
+
+
